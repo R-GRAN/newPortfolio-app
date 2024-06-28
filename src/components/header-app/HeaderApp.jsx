03473389ee763f204c614b895fc04e8aa47c8781
Remy <remy.grangenois@gmail.com>
2024-06-28 03:27:20 +0200

    Header with NavLink active, responsive

---
+++ src/components/header-app/HeaderApp.jsx
@@ -0,0 +1,106 @@
+import "./HeaderApp.scss";
+import laptop from "@/assets/images/laptop.svg";
+
+import { useState } from "react";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+import { Link, NavLink } from "react-router-dom";
+
+function HeaderApp() {
+  const [navIsOpen, setNav] = useState(false);
+
+  function handleNav() {
+    setNav(!navIsOpen);
+  }
+  return (
+    <header className="header">
+      <Link to="/">
+        <img src={laptop} className="header-logo" alt="Logo du portfolio" />
+      </Link>
+      <nav>
+        <ul className="desktop-menu">
+          <li>
+            <NavLink
+              to={"/"}
+              className={({ isActive }) => (isActive ? "active" : "")}
+            >
+              Home
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink
+              to={"/project"}
+              className={({ isActive }) => (isActive ? "active" : "")}
+            >
+              Projets
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={"/skills"}
+              className={({ isActive }) => (isActive ? "active" : "")}
+            >
+              Compétences
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={"/contact"}
+              className={({ isActive }) => (isActive ? "active" : "")}
+            >
+              Contact
+            </NavLink>
+          </li>
+        </ul>
+        <div onClick={handleNav} className="menu-icon">
+          {navIsOpen ? (
+            <AiOutlineClose size={20} />
+          ) : (
+            <AiOutlineMenu size={20} />
+          )}
+        </div>
+      </nav>
+      <ul className={navIsOpen ? "mobile-menu open" : "mobile-menu"}>
+        <h1>Portfolio</h1>
+        <li>
+          <NavLink
+            to={"/"}
+            className={({ isActive }) => (isActive ? "active" : "")}
+            onClick={handleNav}>
+            Home
+          </NavLink>
+        </li>
+
+        <li>
+          <NavLink
+            to={"/project"}
+            className={({ isActive }) => (isActive ? "active" : "")}
+            onClick={handleNav}
+          >
+            Projets
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to={"/skills"}
+            className={({ isActive }) => (isActive ? "active" : "")}
+            onClick={handleNav}
+          >
+            Compétences
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to={"/contact"}
+            className={({ isActive }) => (isActive ? "active" : "")}
+            onClick={handleNav}
+          >
+            Contact
+          </NavLink>
+        </li>
+      </ul>
+    </header>
+  );
+}
+
+export default HeaderApp;
