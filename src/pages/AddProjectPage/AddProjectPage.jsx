3e75f752f482cc146af07ff73b6e8f71c3b830ab
Remy <remy.grangenois@gmail.com>
2024-07-01 03:00:27 +0200

    FakeToken context done, new path for addProject, header behaviours ok

---
+++ src/pages/AddProjectPage/AddProjectPage.jsx
@@ -0,0 +1,9 @@
+import AddProject from "@/components/addProject/AddProject"
+
+function AddProjectPage(){
+    return (<section>
+        <AddProject/>
+    </section>)
+}
+
+export default AddProjectPage
